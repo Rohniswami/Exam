886e8708a49c7d8d679c64409edef21bb19bfbef
Rohniswami <59287010+Rohniswami@users.noreply.github.com>
2019-12-28 17:42:32 +0530

    Add files via upload

--- factorialdemo.js
+++ factorialdemo.js
@@ -1,4 +1,4 @@
-factorial=function(n){
+exports.factorial=function(n){
 fact=1;
 for(i=1;i<=n;i++){
    fact=fact*i;
@@ -6,10 +6,24 @@ for(i=1;i<=n;i++){
 return fact;
 }
 
-addition=function(a,b){
+exports.addition=function(a,b){
 	return a+b;
 	
 }
+//c=setTimeInterval(factorial(5),200);
 
+exports.findsum(){
+	var sum=0;
+	for(i=0;i<arguments.length;i++){
+		sum+=arguments[i];
+		
+	}
+	return sum;
+	
+}
+
+
+
+//clearTimeInterval(c)
 console.log("factorial:"+factorial(5));
 console.log("addition:"+addition(20,30));
